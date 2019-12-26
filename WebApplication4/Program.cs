b96214476b36a85a27270b7d72d3dda07b5f30fd
pranay nandala <pranaynse@outlook.com>
2019-12-26 22:58:43 +0530

    testing

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -20,6 +20,6 @@ namespace WebApplication4
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://life-organizer-app.herokuapp.com/");
+                .UseUrls("http://life-organizer-app.herokuapp.com:80/");
     }
 }
